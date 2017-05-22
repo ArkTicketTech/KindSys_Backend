276fba747edea9a1b3ae897333f7a3b723c5d752
haoliang <1259799112@qq.com>
2017-05-23 01:34:39 +0800

    seperate router

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ var mongoose   = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/kindsys'); // connect to our database
 
-// Models
-var Parent     = require('./models/parent');
-var Parent     = require('./models/teacher');
-
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,73 +22,12 @@ var port = process.env.PORT || 8888;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = require('./routers/router');             // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
-
-// more routes for our API will happen here
-
-// on routes that end in /parents
-// ----------------------------------------------------
-router.route('/parents')
-
-    // create a parent (accessed at POST http://localhost:8080/api/parents)
-    .post(function(req, res) {
-
-        var parent = new Parent();      // create a new instance of the parent model
-        parent.studentName = req.body.studentName;
-        parent.phone = req.body.phone;
-        parent.points = req.body.points;
-        parent.classId = req.body.classId;
-        parent.gender = req.body.gender;
-        // save the parent and check for errors
-        parent.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'parent created!' });
-        });
-
-    })
-
-    // get all the parents (accessed at GET http://localhost:8080/api/parents)
-    .get(function(req, res) {
-        Parent.find(function(err, parents) {
-            if (err)
-                res.send(err);
-
-            res.json(parents);
-        });
-    });
-
-
-router.route('/teachers')
-
-    .post(function(req, res) {
-
-        var teacher = new Teacher();
-        teacher.teacherName = req.body.teacherName;
-        teacher.course = req.body.course;
-        teacher.phone = req.body.phone;
-
-        teacher.save(function(err) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'teacher created!' });
-        });
-
-    })
-
-    .get(function(req, res) {
-        Teacher.find(function(err, teachers) {
-            if (err)
-                res.send(err);
-            res.json(teachers);
-        });
-    });
 
 
 // REGISTER OUR ROUTES -------------------------------
